a73b3e48feb10bdc4f816ed93f1cf5791d0788f9
Neha <nehaoza1307@gmail.com>
2021-02-02 12:59:00 +0000

    Adding base code for vending machine

---
+++ src/main/java/org/collinsongroup/exception/NotSufficientChangeException.java
@@ -0,0 +1,17 @@
+package org.collinsongroup.exception;
+
+public class NotSufficientChangeException extends RuntimeException {
+    private String message;
+
+    public NotSufficientChangeException(String string) {
+        this.message = string;
+    }
+
+    @Override
+    public String getMessage(){
+        return message;
+    }
+
+}
+
+
